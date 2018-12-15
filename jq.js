38002e030852ca15e333bbc14b87a40d5ed9dd3f
AbroudAnis <44729967+AbroudAnis@users.noreply.github.com>
2018-12-15 16:34:55 +0100

    Add files via upload

---
+++ jq.js
@@ -0,0 +1,36 @@
+$("#font-family").change(function() {
+    $(".changeMe").css("font-family", $(this).val());
+  });
+  
+  $("#size").change(function() {
+    $(".changeMe").css("font-size", $(this).val() + "px");
+  });
+  
+  $("#bold").click(function() {
+    $("#P").toggleClass("bold");
+  });
+  $("#italic").click(function() {
+    $("#P").toggleClass("italic");
+  });
+  $("#underline").click(function() {
+    $("#P").toggleClass("underline");
+  });
+  
+  $("#center").click(function() {
+    $("#P").css("text-align","center");
+  });
+  $("#left").click(function() {
+    $("#P").css("text-align","left");
+  });
+  $("#right").click(function() {
+    $("#P").css("text-align","right");
+  });
+  
+  $("#justify").click(function() {
+    $("#P").css("text-align","justify");
+  });
+  
+  $("#color").change(function() {
+    $(".changeMe").css("color", $(this).val() + " ");
+  });
+  
